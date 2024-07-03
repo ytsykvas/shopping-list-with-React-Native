d17cad84cbb90034f8c9694cde50a663a4e4828e
YuriiTsykvas <yurii.t@devmood.tech>
2024-07-03 16:21:13 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.dummyText}>
+        Hello world!
+      </Text>
+      <Text style={styles.dummyText}>
+        ТУТ НЕ ІСНУЄ CSS
+      </Text>
+      <Button title="ЯК ТАК?" />
     </View>
   );
 }
@@ -17,4 +21,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 16,
+    padding: 8,
+    borderWidth: 2,
+    borderBlockColor: 'black'
+  }
 });
