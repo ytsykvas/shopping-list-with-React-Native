214f8b96e28e159000680fe9a951026c7a6322ba
Yurii Tsykvas <yurii.t@devmood.tech>
2024-07-04 16:36:01 +0300

    Improving-the-layout

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder='Type here'/>
         <Button title='submit'/>
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals</Text>
       </View>
     </View>
@@ -16,11 +16,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 80
+    flex: 1,
+    padingTop: 80,
+    paddingHorizontal: 30
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 2
   },
   textInput: {
     borderWidth: 1,
@@ -28,5 +34,8 @@ const styles = StyleSheet.create({
     width: '80%',
     marginRight: 8,
     padding: 8
+  },
+  goalsContainer: {
+    flex: 3
   }
 });
