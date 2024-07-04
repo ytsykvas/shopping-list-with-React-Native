5f5be7204f08ca044646fd9654f81e4cd3a2fa04
ytsykvas <tsykvasyurii@gmail.com>
2024-07-04 21:50:45 +0300

    FlatList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -10,7 +10,9 @@ export default function App() {
   }
 
   function addGoalHandler() {
-    setCourseGoals(currentCourseGoals => [...courseGoals, enteredGoalText]);
+    setCourseGoals(currentCourseGoals => [
+      ...courseGoals, {text: enteredGoalText, id: Math.random().toString()}
+    ]);
   }
 
   return (
@@ -25,17 +27,21 @@ export default function App() {
         <Button title="Add" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <ScrollView>
-          {courseGoals.map((goal) => (
-            <View key={goal} style={styles.goalItem}>
-              <Text style={styles.goalText}>
-                {goal}
-              </Text>
-            </View>
-          ))}
-        </ScrollView>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceHorizontal={false}
+        />
       </View>
-      
     </View>
   );
 }
@@ -74,3 +80,6 @@ const styles = StyleSheet.create({
     color: 'white'
   }
 });
+
+
+
